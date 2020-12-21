cb5b18415ad41545246f7cabfb0e694b606a9a8a
BlackCap <mailhph@gmail.com>
2020-12-21 04:40:34 +0100

    divisibility

--- src/Algebra/Divisibility.hs
+++ src/Algebra/Divisibility.hs
@@ -5,36 +5,44 @@ import Algebra.Set
 import Algebra.Lattice hiding (top)
 import Algebra.PartialOrd
 import Algebra.Heyting
+import Data.Group
 
 
-newtype Divisibility a = Divisibility a
-  deriving newtype
-    ( Semiring, GcdDomain
-    , Euclidean, Ring, Num, Show
+type D = Divisibility
+
+newtype Divisibility a = Divisibility { undiv :: a }
+  deriving stock
+    ( Show
     )
   deriving
     ( Lattice, BoundedJoinSemiLattice, BoundedMeetSemiLattice
-    , Eq, PartialOrd
-    ) via WrappedSet (Divisibility a)
+    , Eq, Ord, PartialOrd, Heyting
+    , Semigroup, Monoid, Group
+    , Semiring, Ring, GcdDomain, Euclidean
+    )
+    via WrappedSet (Divisibility a)
+
+
+divide' :: ∀ a. GcdDomain a => D a -> D a -> Maybe (D a)
+divide' = coerce do divide @a
 
 instance GcdDomain a => Set (Divisibility a)
   where
-    bot       = one
-    top       = zero
-    union     = lcm
-    intersect = gcd
-    disjoint  = coprime
+    bot       = coerce do one     @a
+    top       = coerce do zero    @a
+    union     = coerce do lcm     @a
+    intersect = coerce do gcd     @a
+    disjoint  = coerce do coprime @a
 
     isBot =
       join disjoint
 
     isTop =
-      isBot . plus one
+      isBot . Divisibility . plus one .# undiv
 
     subeq b a
       | isTop a = isTop b -- avoid division by zero
-      | let     = isJust $ divide b a
+      | let     = isJust $ divide' b a
 
     diff a b =
-      fromMaybe bot $ divide a (a ∩ b)
-
+      fromMaybe bot $ divide' a (a ∩ b)
